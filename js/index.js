67f6ea89723a95d7dcf8c76b93f6ea5528f9e152
ZIYU ZHAO <Z248ZHAO@edu.uwaterloo.ca>
2019-05-30 22:52:26 -0400

    add height and width...

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
     var colors = ["#ffffff","#f9d1f6","#d1ffff","#bbb5ff","#fdffb2"];
-    
+    var width = $(window).height();
+    var height = $(window).width();
+    var numOfFlakes = 20;
+    var time = 2; //2 seconds
     function letItSnow(){
-
+        for(var i =0;i<numOfFlakes;i++){
+            
+        }
     }
 
 });
